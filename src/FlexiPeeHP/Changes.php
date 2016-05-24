20c412623a9794c64b735380fd14d34e0df4f989
Vítězslav Dvořák <info@vitexsoftware.cz>
2016-05-24 10:52:30 +0200

    Added class for Changes

---
+++ src/FlexiPeeHP/Changes.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * FlexiPeeHP - Objekt záznamu změn.
+ *
+ * @author     Vítězslav Dvořák <vitex@arachne.cz>
+ * @copyright  (C) 2016 Spoje.Net
+ */
+
+namespace FlexiPeeHP;
+
+class Changes extends FlexiBee
+{
+    /**
+     * Evidence užitá objektem.
+     *
+     * @var string
+     */
+    public $evidence = 'changes';
+
+}
