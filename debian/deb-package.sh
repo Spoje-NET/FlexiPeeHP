5a89691bf9b388d6b00e512a409c81475fdcf234
Vítězslav Dvořák <info@vitexsoftware.cz>
2016-06-25 16:57:43 +0200

    Debian package build update

--- debian/deb-package.sh
+++ debian/deb-package.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 cd ..
+
+PACKAGE=`cat composer.json | grep '"name"' | head -n 1 |  awk -F'"' '{print $4}' | awk -F'/' '{print $2}'`
 VERSION=`cat composer.json | grep version | awk -F'"' '{print $4}'`
 REVISION=`cat debian/revison | perl -ne 'chomp; print join(".", splice(@{[split/\./,$_]}, 0, -1), map {++$_} pop @{[split/\./,$_]}), "\n";'`
-
 CHANGES=`git log -n 1 | tail -n+5`
-dch -b -v $VERSION-$REVISION --package flexipeehp $CHANGES
+dch -b -v $VERSION-$REVISION --package $PACKAGE $CHANGES
 
 debuild -i -us -uc -b
 
-echo $REVISION > debian/revison
+
+LASTVERSION=`cat debian/lastversion`
+if [ $LASTVERSION == $VERSION  ];
+then
+    echo $REVISION > debian/revison
+else
+    echo 0 > debian/revison
+    echo $VERSION > debian/lastversion
+fi
+
+
 cd ..
 ls *.deb
 
-~/bin/publish-deb-packages.sh
+#~/bin/publish-deb-packages.sh
