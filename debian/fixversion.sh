dd024b8afa7c23bb1ed6530abc5130e903f3e429
Vítězslav Dvořák <info@vitexsoftware.cz>
2017-08-30 18:54:27 +0200

    LibVersion script fixed

--- debian/fixversion.sh
+++ debian/fixversion.sh
@@ -4,5 +4,5 @@ REVISION=`cat debian/revision | perl -ne 'chomp; print join(".", splice(@{[split
 echo ${VERSION}.${REVISION}
 sed -i -e '/\"version\"/c\    \"version\": \"'${VERSION}'.'${REVISION}'",' debian/flexipeehp/usr/share/php/FlexiPeeHP/composer.json
 
-sed -i -e "/static public \$libVersion/c\    static public \$libVersion = '${VERSION}.${REVISION}';" debian/flexipeehp/usr/share/php/FlexiPeeHP/FlexiBeeRO.php
-sed -i -e "/static public \$libVersion/c\    static public \$libVersion = '${VERSION}.${REVISION}';" src/FlexiPeeHP/FlexiBeeRO.php
+sed -i -e "/public static \$libVersion/c\    public static \$libVersion = '${VERSION}.${REVISION}';" debian/flexipeehp/usr/share/php/FlexiPeeHP/FlexiBeeRO.php
+sed -i -e "/public static \$libVersion/c\    public static \$libVersion = '${VERSION}.${REVISION}';" src/FlexiPeeHP/FlexiBeeRO.php
